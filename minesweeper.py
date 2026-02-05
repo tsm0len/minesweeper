54f45d6325c638a5481b01b08df4a095bfd8d378
sm0L <122048823+tsm0len@users.noreply.github.com>
2026-02-05 19:33:41 +0100

    Commented, improved starting point, added timer

--- minesweeper.py
+++ minesweeper.py
@@ -6,11 +6,19 @@ def setup():
     screen = turtle.Screen()
 
     n = screen.textinput("Size","Board Size (e.g. 12x10): ")
-    n = "12x10" if n == None else n
-    size_x, size_y = list(int(i) for i in n.split("x"))
+    n = "12x10" if n == None else n #in case of closing input window
+    try:
+        size_x, size_y = list(int(i) for i in n.split("x"))
+    except:
+        size_x, size_y = 12, 10
+        print("Incorrect value, switched to default 12x10")
+    if size_x*size_y<6:
+        size_x, size_y = 12, 10
+        print("Incorrect value, switched to default 12x10")
+        
     bombs_left = size_x*size_y//6
-    
-    screen.setup(width=(size_x+0.8)*size, height=(size_y+1.8)*size)
+
+    screen.setup(width=(size_x+0.8)*size, height=(size_y+1.8)*size) #some extra margin
     screen.title("Minesweeper")
     t = turtle.Turtle()
     return t, screen, size, size_x, size_y, bombs_left
@@ -23,88 +31,90 @@ screen.colormode(255)
 t.hideturtle()
 
 #generates number board and state board
-def gen_board(width:int, height:int, amount:int) -> tuple[list[list[int]], list[list[str]], tuple[int, int]]:
+def gen_board(width:int, height:int, bomb_count:int) -> tuple[list[list[int]], list[list[str]], tuple[int, int]]:
 
     board_num = [[0]*width for _ in range(height)]
     board_state = [["closed"]*width for _ in range(height)] #closed, flagged, open
 
-    available = []
-    for y in range(height):
+    available = [] #creating list of all cells in order to avoid double selection
+    for y in range(height): #also used in selecting starting point 
         for x in range(width):
-            available.append((x, y))
+            available.append((x, y)) 
             
-    for _ in range(amount):
+    for _ in range(bomb_count):
         selected = random.choice(available)
-        board_num[selected[1]][selected[0]] = -1
+        board_num[selected[1]][selected[0]] = -1 #-1 is bomb
         available.remove(selected)
         
-        for j in range(-1, 2):
+        for j in range(-1, 2): 
             for i in range(-1, 2):
-                xx, yy = selected[0]+i, selected[1]+j
+                xx, yy = selected[0]+i, selected[1]+j #for every cell neighbouring new bomb
                 if xx!=-1 and yy!=-1 and xx<size_x and yy<size_y:       
-                    if board_num[yy][xx]!=-1:
-                        board_num[yy][xx]+=1
-    zero_cells = []
+                    if board_num[yy][xx]!=-1: 
+                        board_num[yy][xx]+=1 #increasing number
+                        
+    
+    min_value = 8
     for cell in available:
-        if board_num[cell[1]][cell[0]] == 0:
-            zero_cells.append(cell)
-    starting_cell = random.choice(zero_cells)
-    # board_state[starting_cell[1]][starting_cell[0]] = "open"
-    # 
+        min_value = min(board_num[cell[1]][cell[0]], min_value) #in case there are no cells with 0 bombs nearby, game starts with lowest possible number
+        if min_value == 0:
+            break #there aint no values under 0 
+
+    possible_cells = []
+    for cell in available:
+        if board_num[cell[1]][cell[0]] == min_value: #gathering all the cells with lowest value
+            possible_cells.append(cell)
+            
+    starting_cell = random.choice(possible_cells)
+    
     return board_num, board_state, starting_cell
 
-
 board_num, board_state, starting_cell = gen_board(size_x, size_y, bombs_left)
-
 
 def win_detection(board_num=board_num, board_state=board_state) -> bool:
     for y in range(size_y):
         for x in range(size_x):
-            if board_num[y][x]!=-1 and board_state[y][x] != "open":
+            if board_num[y][x]!=-1 and board_state[y][x] != "open": #if any of non-bomb cells are not open return false
                 return False
     return True
 
-mouse_pos = [0, 0]
-def l_click(x, y) -> None:
+def l_click(x, y) -> None: #just to differentiate left click and right click
     click_action(x, y, "l")
 def r_click(x, y) -> None:
     click_action(x, y, "r")
     
-    
-def click_action(x:float, y:float, btn, cell=None) -> None:
-    global mouse_pos
+def click_action(x:float, y:float, btn, cell=None) -> None: #cell used only in recursive opening 0 cells
     global board_state
     global bombs_left
-    mouse_pos = [x, y]
     if cell==None:
-        cell = [int(x/size+size_x/2), int(size_y-((y-size/2)/size+size_y/2)-1)]
+        cell = [int(x/size+size_x/2), int(size_y-((y-size/2)/size+size_y/2)-1)] #else is just where player clicked
 
     match board_state[cell[1]][cell[0]]:
         case "closed":
             if btn == "r":
-                board_state[cell[1]][cell[0]] = "flagged"
+                board_state[cell[1]][cell[0]] = "flagged" #tagging as flagged
                 bombs_left -= 1
             else:
                 board_state[cell[1]][cell[0]] = "open"
-                if board_num[cell[1]][cell[0]] == 0:
+                if board_num[cell[1]][cell[0]] == 0: #if opened 0 cell, it opens every cell around it
                     for j in range(-1, 2):
                         for i in range(-1, 2):
                             xx, yy = cell[0]+i, cell[1]+j
                             if xx!=-1 and yy!=-1 and xx<size_x and yy<size_y:
-                                click_action(0, 0, "l", [xx, yy])
+                                click_action(0, 0, "l", [xx, yy]) #using recursion to click all the cells
         case "flagged":
             if btn == "r":
-                board_state[cell[1]][cell[0]] = "closed"
+                board_state[cell[1]][cell[0]] = "closed" #unflagging
                 bombs_left += 1
 
-    draw_board_on_screen(bombs_left=bombs_left)
+    draw_board_on_screen(bombs_left=bombs_left) #doesnt work without specified bomb count for some reason
     if board_num[cell[1]][cell[0]] == -1 and btn == "l" and board_state[cell[1]][cell[0]]=="open":
         exit()
+        
     if bombs_left == 0:
         if win_detection():
-            print("YOU WON")
             screen.reset()
-            t.write("You won!", align="center", font=("Courier New", (int(size/3)), "bold"))
+            t.write(f"You won! Time: {round(time.time()-start_time, 2)}", align="center", font=("Courier New", (int(size/3)), "bold"))
             turtle.exitonclick()
 
 COLORS = {
@@ -123,7 +133,7 @@ COLORS = {
     "8":"#494848"
 }
 
-def draw_board_on_screen(board_num=board_num, board_state=board_state, size_x=size_x, size_y=size_y, cell_size=size, bombs_left=bombs_left) -> None:
+def draw_board_on_screen(board_num=board_num, board_state=board_state, size_x=size_x, size_y=size_y, cell_size=size, bombs_left=bombs_left) -> None: #specifying variables because why not (optimization or sum)
     t.clear()
     screen.tracer(False)
     for y in range(size_y):
@@ -161,10 +171,10 @@ def draw_board_on_screen(board_num=board_num, board_state=board_state, size_x=si
     screen.update()
 
 draw_board_on_screen() #initial rendering
-click_action(0, 0, "l", starting_cell)
+click_action(0, 0, "l", starting_cell) #and clicking first cell chosen earlier
+start_time = time.time()
 
-
-turtle.onscreenclick(l_click, 1)
+turtle.onscreenclick(l_click, 1) #binding left click and right click
 turtle.onscreenclick(r_click, 3)
 
 turtle.listen()
